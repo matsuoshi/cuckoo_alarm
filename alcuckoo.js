5d5e788ddb5137ddb2cc32813cad9f7c09482cb6
h.matsuo <matsuoshi@gmail.com>
2020-09-25 08:35:55 +0900

    update alarm.mp3

--- alcuckoo.js
+++ alcuckoo.js
@@ -1,9 +1,8 @@
 var per_minutes = 30
-var cuckoo = new Audio(chrome.runtime.getURL("alcuckoo.mp3"))
+var cuckoo = new Audio(chrome.runtime.getURL("alarm.mp3"))
 
 function checkAlarm()
 {
-  console.log(new Date())
   if (new Date().getMinutes() % per_minutes === 0) {
     cuckoo.play()
   }
@@ -11,7 +10,7 @@ function checkAlarm()
 
 setTimeout(function(){
   checkAlarm()
-  chrome.alarms.create('alcuckoo', {"periodInMinutes": 1})
+  chrome.alarms.create("alcuckoo", {"periodInMinutes": 1})
 }, (60 - new Date().getSeconds()) * 1000)
 
 chrome.alarms.onAlarm.addListener(checkAlarm)
