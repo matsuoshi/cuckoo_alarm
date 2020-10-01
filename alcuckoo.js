6e50d728a9839cb6b7abe5f492c8f7eebbc1e482
h.matsuo <matsuoshi@gmail.com>
2020-10-01 15:36:11 +0900

    add toggle button (browser_action)

--- alcuckoo.js
+++ alcuckoo.js
@@ -1,20 +1,53 @@
-var per_minutes = 30
-var cuckoo = new Audio(chrome.runtime.getURL("alarm.mp3"))
-
+/*
+ * cuckoo alarm
+ */
 console.log("start")
+const per_minutes = 30
+let is_disabled = false
+
+
+/*
+ * init
+ */
+const waitingSeconds = 60 - new Date().getSeconds()
+
+setTimeout(() => {
+  checkAlarm()
+  chrome.alarms.create("alcuckoo", {"periodInMinutes": 1})
+  chrome.alarms.onAlarm.addListener(checkAlarm)
+  console.log("alarm created")
+}, waitingSeconds * 1000)
+
+
+/*
+ * alarm
+ */
+const cuckoo = new Audio(chrome.runtime.getURL("alarm.mp3"))
 
 function checkAlarm()
 {
-  if (new Date().getMinutes() % per_minutes === 0) {
-    cuckoo.play()
-    console.log("cuckoo!")
+  if (is_disabled) {
+    return false
   }
+
+  if (new Date().getMinutes() % per_minutes !== 0) {
+    return false
+  }
+
+  cuckoo.play()
+  console.log("cuckoo!")
+  return true
 }
 
-setTimeout(function(){
-  checkAlarm()
-  chrome.alarms.create("alcuckoo", {"periodInMinutes": 1})
-  console.log("alarm created")
-}, (60 - new Date().getSeconds()) * 1000)
 
-chrome.alarms.onAlarm.addListener(checkAlarm)
+/*
+ * button clicked
+ */
+chrome.browserAction.onClicked.addListener(() => {
+  is_disabled = ! is_disabled
+
+  let icon = is_disabled ? 'icon_disabled' : 'icon128'
+  chrome.browserAction.setIcon({path: `icons/${icon}.png`})
+
+  console.log(is_disabled)
+})
